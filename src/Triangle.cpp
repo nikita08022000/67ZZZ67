199b87b1603bfbf1d63d35f74bf526f2eb1555dc
master-baiter1487 <harryhopkins6005@gmail.com>
2026-04-07 23:49:49 +0300

    Triangle

---
+++ src/Triangle.cpp
@@ -0,0 +1,20 @@
+class Triangle : public I2DShape {
+private:
+    double a, b, c;
+public:
+    Triangle(double side1, double side2, double side3)
+        : a(side1), b(side2), c(side3) {}
+    
+    double area() const {
+        double p = (a + b + c) / 2.0;
+        return sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+    
+    double perimeter() const {
+        return a + b + c;
+    }
+    
+    const char* name() const {
+        return "Triangle";
+    }
+};
