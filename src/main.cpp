df9055eeced95300462dbe0880fe4ca74212d49c
nikita08022000 <meleshkonikita08022010@mail.ru>
2026-04-14 23:54:28 +0300

    ...

---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <cmath>
+#include "Circle.cpp"
+#include "Cube.cpp"
+#include "NPolygons.cpp"
+#include "Sphere.cpp"
+#include "Square.cpp"
+#include "trap.cpp"
+#include "Triangle.cpp"
+
+
+
+
+int main() {
+    std::cout << "2D FIGURES\n\n";
+    
+    Square square(5.0);
+    Trapezoid trapezoid(8.0, 5.0, 4.0, 4.0, 3.0);
+    Triangle triangle(3.0, 4.0, 5.0);
+    Circle circle(3.0);
+    NPolygon hexagon(6, 4.0);
+    
+    print2DShape(square);
+    print2DShape(trapezoid);
+    print2DShape(triangle);
+    print2DShape(circle);
+    print2DShape(hexagon);
+    
+    std::cout << "3D FIGURES\n\n";
+    
+    Cube cube(4.0);
+    Sphere sphere(3.0);
+    
+    print3DShape(cube);
+    print3DShape(sphere);
+    
+    std::cout << "MASSIVE OF FIGURES\n\n";
+    
+    I2DShape* shapes2D[] = {
+        new Square(6.0),
+        new Circle(2.5),
+        new Triangle(5.0, 5.0, 6.0),
+        new NPolygon(5, 3.0)
+    };
+    
+    for (int i = 0; i < 4; i++) {
+        print2DShape(*shapes2D[i]);
+        delete shapes2D[i];
+    }
+    
+    I3DShape* shapes3D[] = {
+        new Cube(2.5),
+        new Sphere(4.0)
+    };
+    
+    for (int i = 0; i < 2; i++) {
+        print3DShape(*shapes3D[i]);
+        delete shapes3D[i];
+    }
+    
+    return 0;
+}
